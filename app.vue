6a5287354679631f3e59df9fa252aa2133e38122
Anaswara T Rajan <anaswaratrajan@gmail.com>
2024-06-08 23:44:01 +0530

    add sass and styling

--- app.vue
+++ app.vue
@@ -1,26 +1,29 @@
 <template>
   <div>
-    <!-- <h1 style="align: center;">Your life in Weeks</h1>
-
-    <div>Input DOB (datetime picker)</div>
-    <div>input life expectancy (multiples of grid width)</div> -->
-
-    <div>
-      <div class="container">
-        <div ref="life" class="life-grid">
-          <div
-            v-for="(r, i) in lifeGrid[0]"
-            :key="`r${lifeGrid[0] * i}`"
-            class="life-row"
-          >
+    <div class="app-container">
+      <div class="wrapper-container">
+        <h1>Your life in Weeks</h1>
+        <div class="grid-container">
+          <div ref="life" class="life-grid">
             <div
-              v-for="(v, j) in lifeGrid[1]"
-              :key="lifeGrid[0] * i + j"
-              class="dot"
-              v-bind:style="[lifeGrid[0] * i + j > (lifeExpectancy - daysLeft) ? { background: 'red' }: { background: 'grey' }]"></div>
+              v-for="(r, i) in lifeGrid[0]"
+              :key="`r${lifeGrid[0] * (i + 1)}`"
+              class="life-row"
+            >
+              <div
+                v-for="(v, j) in lifeGrid[1]"
+                :key="`r${lifeGrid[0] * (i + 1) + (j + 1)}`"
+                class="week-dot"
+                v-bind:style="[
+                  lifeGrid[1] * i + (j + 1) > lifeExpectancy - weeksLeft
+                    ? { background: 'red' }
+                    : { background: 'grey' },
+                ]"
+              ></div>
+            </div>
           </div>
+          <!-- grid past days in grey, future days in red, today -->
         </div>
-        <!-- grid past days in grey, future days in red, today -->
       </div>
     </div>
   </div>
@@ -32,10 +35,6 @@ const appConfig = useAppConfig();
 var lifeGrid = [50, 100];
 var lifeExpectancy = 0; // in weeks
 var weeksLeft = 0;
-
-const setDoB = function () {
-  console.log("dob set")
-}
 
 const loadLifeGrid = function () {
   console.log("life grid loaded from localstorage");
@@ -49,25 +48,26 @@ onBeforeMount(() => {
 });
 </script>
 
-<style>
-.container {
-  display: flex;
-  justify-content: center;
+<style lang="scss">
+.grid-container {
+  align-self: center;
+  .life-grid {
+    width: fit-content;
+  }
+  .life-row {
+    line-height: 0;
+  }
+  .week-dot {
+    line-height: 0;
+    border-radius: 50%;
+    width: 0.4rem;
+    height: 0.4rem;
+    margin: 0.1rem;
+    display: inline-flex;
+    flex-direction: column;
+  }
 }
-.life-grid {
-  width: fit-content;
-}
-.life-row {
-  line-height: 0;
-}
-.dot {
-  background: grey;
-  width: 0.5rem;
-  height: 0.5rem;
-  border-radius: 50%;
-  margin: 0.1rem;
-  line-height: 0;
-  display: inline-flex;
-  flex-direction: column;
+h1 {
+  align-self: center;
 }
 </style>
